086a99f3b94169e7e407b35e88d984a93759ff80
Troshin-ES(Work PC) <troshin-es@mail.ru>
2024-09-02 15:46:46 +0300

    v2

--- django_menu/menu/models.py
+++ django_menu/menu/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Menu(models.Model):
-    name = models.CharField('Название пункта меню', max_length=100)
+    name_menu = models.CharField('Название меню', max_length=100)
+    name_item = models.CharField('Название пункта меню', max_length=100)
     url = models.CharField('Ссылка', max_length=255)
     parent = models.ForeignKey('self',
                                on_delete=models.CASCADE,
@@ -13,10 +14,10 @@ class Menu(models.Model):
                                related_name='children')
 
     def __str__(self):
-        return str(self.name)
+        return f'{self.name_menu}: {self.name_item}'
 
     class Meta:
-        ordering = ('id',)
+        ordering = ('name_menu',)
         verbose_name = 'Пункт меню'
         verbose_name_plural = 'Пункты меню'
 
--- django_menu/menu/templatetags/common_tags.py
+++ django_menu/menu/templatetags/common_tags.py
@@ -5,10 +5,18 @@ register = template.Library()
 
 # show_menu(None)
 @register.inclusion_tag('menu.html')
-def show_menu(nodes, prevision_node):
+def draw_menu(name_menu, *args
+              # nodes, prevision_node
+              ):
+    print(args)
     if nodes is None:
-        nodes = Menu.objects.all()
+        nodes = Menu.objects.filter(name_menu=name_menu)
         top_nodes = [node for node in nodes if node.parent is None]
         return {'nodes': top_nodes}
-    return {'nodes': nodes,
+    return {'name_menu': name_menu,
+            # 'request': request,
+            # 'current_url': current_url,
+            'nodes': nodes,
             'prevision_node': prevision_node}
+
+
